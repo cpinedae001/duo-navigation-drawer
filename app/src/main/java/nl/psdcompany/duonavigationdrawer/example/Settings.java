61f50bf2169597cd80d1aa7ea7695b06e33bebe9
Cristhian Pineda <cpineda@mobilges.com>
2020-05-06 18:11:41 -0600

    Bluetooth
    se agrega clases bluetooth

--- app/src/main/java/nl/psdcompany/duonavigationdrawer/example/Settings.java
+++ app/src/main/java/nl/psdcompany/duonavigationdrawer/example/Settings.java
@@ -1,17 +1,26 @@
 package nl.psdcompany.duonavigationdrawer.example;
 
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import Utiliti.ConnectedThread;
 
 
 /**
@@ -19,13 +28,19 @@ import android.widget.ListView;
  */
 public class Settings extends Fragment {
 
-
-    private Button scan;
-    private Button off;
-    private Button PairedBtn;
-    private Button discover;
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+    private TextView mBluetoothStatus;
+    private Button onBtn, offBtn, discover, pairedBtn;
     private CheckBox checkboxLED1;
-    private ListView devicesListView;
+    private ListView listViewDevices;
+    private ArrayAdapter<String> mBTArrayAdapter;
+
+    private Handler mHandler; // Our main handler that will receive callback notifications
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
+
+    BluetoothAdapter mBTAdapter;
 
     public Settings() {
         // Required empty public constructor
@@ -38,9 +53,28 @@ public class Settings extends Fragment {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         view.setVerticalScrollBarEnabled(true);
+        onBtn = view.findViewById(R.id.onBtn);
+        offBtn = view.findViewById(R.id.offBtn);
+        discover = view.findViewById(R.id.discover);
+        pairedBtn = view.findViewById(R.id.pairedBtn);
+        listViewDevices = view.findViewById(R.id.devicesListView);
+        mBluetoothStatus = view.findViewById(R.id.bluetoothStatus);
 
 
         return view;
+    }
+
+    private void bluetoothOn(View view){
+        if (!mBTAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            mBluetoothStatus.setText("Bluetooth enabled");
+            Toast.makeText(getContext(),"Bluetooth turned on",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(getContext(),"Bluetooth is already on", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
